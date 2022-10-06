af078c3fffe6e2eaa2008e6add1097d8791021d2
jeonga-Son <jjeong0106@gmail.com>
2022-10-06 12:54:29 -0400

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by jeonga Son on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
